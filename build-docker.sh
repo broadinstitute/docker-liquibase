f946edef5930fd3cf2778020508e07654e1be329
Henry Ferrara <hjferrara@bynara.com>
2017-12-11 13:24:09 -0500

    start of building a generic docker

--- build-docker.sh
+++ build-docker.sh
@@ -8,13 +8,41 @@
 #  - MYSQL-CONN_VERSION
 
 # If any of these environment vars are not set the build will exit with failure
+JDK_VERSION=${JDK_VERSION:-"8"}
+VAULT_VERSION=${VAULT_VERSION:-"0.7.0"}
+LIQUIBASE_VERSION=${LIQUIBASE_VERSION:-"3.5.3"}
+MYSQL-CONN_VERSION=${MYSQL-CONN_VERSION:-"5.1.40"}
 
 # the Dockerfile template is required to exist in this repo
 
 # Docker Hub container name
 REPO="broadinstitute/liquibase"
 
+# build number hack
+BUILD_NUM=${BUILD_NUM:-0}
+BUILD_TAG=${BUILD_TAG:-"build"}
+
 # TODO
 # check that all vars are set
+
+if [ -z "${JDK_VERSION}" -o -z "${VAULT_VERSION}" -o -z "${LIQUIBASE_VERSION}" -o -z "${MYSQLCONN_VERSION}" ]
+then
+    echo "ERROR: Must specify versions that will be part of build"
+    exit 1
+fi
+
+# build Dockerfile from template
+
+sed -e "s;LIQUIBASE_VERSION_TAG;${LIQUIBASE_VERSION};" -e "s;MYSQLCONN_VERSION_TAG;${MYSQLCONN_VERSION};" -e "s;VAULT_VERSION_TAG;${VAULT_VERSION};" -e "s;JDK_VERSION_TAG;${JDK_VERSION};" < Dockerfile.tmpl > Dockerfile
+
+# build docker
+
+docker build -t ${REPO}:${BUILD_TAG}.${BUILD_NUM} .
+# need to check return status on build
+
+# rm Dockerfile after build
+rm -f Dockerfile
+
+docker tag ${REPO}:${BUILD_TAG}.${BUILD_NUM} ${REPO}:liquibase-${LIQUIBASE_VERSION}
 
 
---
+++ run.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# get progname
+
+# /opt/liquibase/liquibase --driver=com.mysql.jdbc.Driver --classpath=/opt/liquibase/mysql-connector-java-5.1.40-bin.jar  --changeLogFile=db/cloud_metrics/liquibase/changesets/baseline.xml --url="jdbc:mysql://127.0.0.1:3306/metrics" --username=jcarey --password=test  migrate
+
+# this script will run a changeset manually
+
+# changesets should appear under the directory in the var CHGLOG_DIR
+# NOTE: you need to ensure that any path referenced by the changelog.xml is 
+#  resolveable inside docker.  Best practise is for changesets to be referenced
+#  using relative paths
+
+# script 
+
+# todo check that environment vars are set
+
+ENV=${ENV:-""}
+APP_PROJ=${APP_PROJ:-""}
+APP_NAME=${APP_NAME:-""}
+DB_NAME=${DB_NAME:-"${APP_NAME}"}
+DB_HOST=${DB_HOST:-""}
+DB_USER=${DB_USER:-""}
+DB_PASSWORD=${DB_PASSWORD:-""}
+CHGLOG_DIR=${CHGLOG_DIR:-"/changelog"}
+LOG_DIR=${LOG_DIR:-"/working"}
+LOG_LEVEL=${LOG_LEVEL:-"debug"}
+LOG_OPTS=${LOG_OPTS:-""}
+USE_SSL=${USE_SSL:-1}
+
+if [ ! -z "${LOG_LEVEL}" ]
+then
+   # if log level set also set logfile output
+   LOG_OPTS="--logLevel=${LOG_LEVEL} --logFile=${LOG_DIR}/log.out"
+fi
+
+DB_URL="jdbc:mysql://${DB_HOST}:3306/${DB_NAME}"
+# add SSL if set
+
+if [ "${SSL}" -eq 1 ]
+then
+  DB_URL="${DB_URL}?useSSL=true&requireSSL=true"
+  vault read --field=keystore "secret/dsde/${APP_PROJ}/${ENV}/${DB_NAME}/${DB_NAME}-mysql" | base64 -d > /tmp/keystore
+  vault read --field=truststore "secret/dsde/${APP_PROJ}/${ENV}/${DB_NAME}/${DB_NAME}-mysql" | base64 -d > /tmp/truststore
+ JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.keyStore=/tmp/keystore -Djavax.net.ssl.keyStorePassword=changeit -Djavax.net.ssl.trustStore=/tmp/truststore -Djavax.net.ssl.trustStorePassword=changeit"
+  export JAVA_OPTS
+fi
+
+# DB_USER=`vault read --field=admin_user "secret/dsde/${APP_PROJ}/${ENV}/${DB_NAME}/secrets"`
+# DB_PASS=`vault read --field=admin_password "secret/dsde/${APP_PROJ}/${ENV}/${DB_NAME}/secrets"`
+
+echo "Running liquibase using the following command: "
+echo "JAVAOPTS: ${JAVA_OPTS}"
+echo "liquibase  --driver=com.mysql.jdbc.Driver ${LOG_OPTS} --changeLogFile=${CHGLOG_DIR}/changelog.xml --url="${DB_URL}" --username=${DB_USER} --password=XXXXXXX  migrate"
+
+liquibase  --driver=com.mysql.jdbc.Driver ${LOG_OPTS} --changeLogFile=${CHGLOG_DIR}/changelog.xml --url="${DB_URL}" --username=${DB_USER} --password=${DB_PASS}  migrate
+
